c2cdd3d5ec5b73fc3d0c93c23b880188d6c3c66f
fabexia <fabexia@fab-exia>
2017-07-03 15:31:02 +0200

    Architecture MVC of the project

--- Hunger Games of Mother Russia/src/Main/main.java
+++ Hunger Games of Mother Russia/src/Main/main.java
@@ -1,12 +1,15 @@
+package Main;
+
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
 		System.out.println("lorem ispum vodka russia");
 		System.out.println("Russian version of hello world");
-		
+		String str = sc.nextLine();
+		System.out.println("Vous avez tapez : " + str);
 	}
-
 }
